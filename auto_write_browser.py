07cea336d18efb0dc3865ce1f470b419b81835cd
apo <fukick@gmail.com>
2022-04-24 16:49:40 +0900

    コメントを整理

--- auto_write_browser.py
+++ auto_write_browser.py
@@ -116,16 +116,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-#セルに既に文字列があった場合
-#まず文字列を取得し，ハイフンの後ろのみを変更
-# 8:30 - 19:00 勉強のため
-
-#セルに文字列がなかった場合
-#時間＋ハイフンを代入
-#8:30 - など
-
-#send to cell a timestamp
-# sheet.update_cell(
-#     12, 8,
-#     "8:50-19:30 ミーティングのため")  #(row, col(H) , "words that you send to cell")
